de18007c94d3e790b4693e0c1f233517aa5c5aa9
Clement Owireku-Bogyah <clement.owireku-bogyah@amalitech.com>
2024-04-22 16:37:07 +0000

    chores:setting up

---
+++ src/main/java/com/clement/fokuz/FokuzApplication.java
@@ -0,0 +1,13 @@
+package com.clement.fokuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FokuzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FokuzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clement/fokuz/FokuzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clement.fokuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FokuzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
